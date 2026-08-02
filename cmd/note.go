4a754445731688b8971e8c697294469f6336cf34
Golu <140903720+GoluScriptMage@users.noreply.github.com>
2026-08-02 09:02:58 +0530

    feat: implemented get store path & read json file

--- cmd/note.go
+++ cmd/note.go
@@ -2,17 +2,29 @@ package cmd
 
 import (
 	"fmt"
-
 	"github.com/spf13/cobra"
+	"os"
+	"path/filepath"
 )
 
 var noteCmd = &cobra.Command{
 	Use:   "note [text]",
 	Short: "Note is command to save notes",
 	Run: func(cmd *cobra.Command, args []string) {
+		// Check if any arguments were provided
 		if len(args) != 0 {
 			fmt.Println(args[0])
 		}
+
+		// Get the cwd and read the file
+		currentDir, _ := os.Getwd()
+		fsPath := filepath.Join(currentDir, "store", "json.go")
+
+		bytes, err := os.ReadFile(fsPath)
+		if err != nil{
+			fmt.Println("Error reading file:", err)
+		}
+		fmt.Println(string(bytes))
 	},
 }
 
