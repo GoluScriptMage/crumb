2b335c59a33c2e1575d7fbb2821df39893032d63
Golu <140903720+GoluScriptMage@users.noreply.github.com>
2026-08-04 10:25:18 +0530

    feat: crumb next & next clear cmd with save and clear

--- cmd/next.go
+++ cmd/next.go
@@ -1,1 +1,50 @@
 package cmd
+
+import (
+	"crumb/store"
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var nextCmd = &cobra.Command{
+	Use:   "next [text]",
+	Short: "Next is command to save next actions",
+	Run: func(cmd *cobra.Command, args []string) {
+		data, err := store.ReadData()
+
+		if err != nil {
+			fmt.Println("Error read data")
+			return
+		}
+		// For displaying current focus
+		if len(args) == 0 {
+			if data.Next == "" {
+				fmt.Println("No current focus set.")
+			} else {
+				fmt.Println("Current focus: ", data.Next)
+			}
+			return
+		}
+
+		// For clearing focus or saving new focus
+		if args[0] == "clear" {
+			data.Next = ""
+			fmt.Println("Focus cleared: ")
+		} else {
+			data.Next = args[0]
+			fmt.Println("Focus updated")
+		}
+
+		// Update the data in the JSON file
+		err = store.WriteData(data)
+
+		if err != nil {
+			fmt.Println("Cannot write data")
+			return
+		}
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(nextCmd)
+}
